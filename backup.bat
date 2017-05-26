77a39771850157bdfbe6d4d292131b38e79194b9
Jason Dixon <jason.dixon.email@gmail.com>
2017-05-27 01:32:52 +1200

    backup working

--- backup.bat
+++ backup.bat
@@ -1,43 +1,60 @@
 @echo off
+setlocal enableextensions enabledelayedexpansion
+
+REM Our messages. Change if you want a different language.
+set prompt=What have you achieved?
+set creatingbackup=Creating backup.
+set nosave=Cannot find save file.
+set createfolder=Creating backup folder.
+set backupexsits=Backup already exists.
+set success=Backup successful.
 
 REM Our variables!
 set backup=%~dp0Backups\
 set savegame=%appdata%\DarkSoulsIII\
 
 REM Quit if savefile cannot be found.
-if not exist %savegame% (
-  echo Cannot find save file.
+if not exist "%savegame%" (
+  echo %nosave%
   pause
   exit
   )
 
 REM Create a backup folder if one cannot be found.
-if not exist %backup% (
-  echo Creating backup folder.
+if not exist "%backup%" (
+  echo %createfolder%
   mkdir %backup%
   )
 
 REM Request a name for our backup.
-set /p name="What did you do last? -> "
+set /p comment="%prompt% -> "
 
-REM Get the current date and time.
-For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%b-%%a)
-For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a%%b)
-echo %mydate%_%mytime%
+REM Sanitize input.
+REM https://stackoverflow.com/questions/19855925/removing-non-alphanumeric-characters-in-a-batch-variable
+set _input="%date%_%time%_%comment%"
+set "_output="
+:loop
+if not defined _input goto endLoop
+set "_buf=!_input:~0,1!"
+set "_input=!_input:~1!"
+echo "!_buf!"|findstr /i /r /c:"[a-z 0-9_]" > nul && set "_output=!_output!!_buf!"
+goto loop
+:endLoop
+set latest=%backup%!_output!\
 
+REM Check folder isn't already there.
+if exist "%latest%" (
+  echo %backupexsits%
+  pause
+  exit
+  )
 
-for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
-set ldt=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2% %ldt:~8,2%:%ldt:~10,2%:%ldt:~12,6%
-echo Local date is [%ldt%]
+REM Make the folder and copy over game files.
+echo %creatingbackup%
+mkdir "%latest%"
+xcopy /s /e /q "%savegame%*" "%latest%"
+echo %success%
 
-pushd "%temp%"
-makecab /D RptFileName=~.rpt /D InfFileName=~.inf /f nul >nul
-for /f "tokens=3-7" %%a in ('find /i "makecab"^<~.rpt') do (
-   set "current-date=%%e-%%b-%%c"
-   set "current-time=%%d"
-   set "weekday=%%a"
-)
-del ~.*
-popd
-echo %weekday% %current-date% %current-time%
-pause
+REM Pause if executed directly.
+REM https://stackoverflow.com/questions/3551888/pausing-a-batch-file-when-double-clicked-but-not-when-run-from-a-console-window
+ECHO %cmdcmdline% | findstr /i /c:"%~nx0" >NUL 2>&1 && PAUSE
